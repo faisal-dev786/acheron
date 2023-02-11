30d69278fa21d3f32745fedfb9423107e384fe98
Faisal <fasinadem108@gmail.com>
2023-02-11 12:04:00 +0500

    sss

--- src/components/Banner2.js
+++ src/components/Banner2.js
@@ -4,7 +4,7 @@ const Banner2 = () => {
   return (
     <>
     <div className='wrapper grid grid-cols-1 sm:grid-cols-3 sm:py-32 justify-items-center items-center'>
-        <div><img src="/banner2_Img.png" alt="" /></div>
+        <div><img src="/bannerImg.png" alt="" /></div>
         <div><img className='' src="/Line.png" alt="" /></div>
         <div><p>Over 100+ Exchange Listings available to our clients </p></div>
     </div>
